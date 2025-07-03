6477b56019ec1a9f1df6e48b15e6ff8554f01a62
Tanvir Raihan <tanvirraihan@finder-lbs.com>
2025-07-03 16:53:05 +0600

    added model schema for money entry

---
+++ addons/money_app/models/money_entry.py
@@ -0,0 +1,26 @@
+from odoo import models, fields, api
+
+
+class MoneyEntry(models.Model):
+    _name = "money.entry"
+    _description = "Money Entry"
+
+    name = fields.Char(string="Description", required=True)
+    date = fields.Date(string="Date", default=fields.Date.today)
+    amount = fields.Float(string="Amount", required=True)
+    type = fields.Selection(
+        [
+            ('income', 'Income'),
+            ('expense', 'Expense')
+        ],
+        string="Type",
+        required=True
+    )
+    balance = fields.Float(compute='_compute_balance', string="Balance", readonly=True)
+
+    @api.depends('type', 'amount')
+    def _compute_balance(self):
+        for record in self:
+            income = sum(self.search([('type', '=', 'income')]).mapped('amount'))
+            expenses = sum(self.search([('type', '=', 'expense')]).mapped('amount'))
+            record.balance = income - expenses

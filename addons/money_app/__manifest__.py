7ca4471808cfa44fd86e9c358b2e1413ed19b797
Tanvir Raihan <tanvirraihan@finder-lbs.com>
2025-07-03 09:40:45 +0600

    added addons

---
+++ addons/money_app/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Money App',
+    'version': '1.0',
+    'category': 'Accounting',
+    'summary': 'Manage your income and expense with ease',
+    'description': """
+        NA
+    """,
+    'author': "Tanvir",
+    'website': 'https://matacentric.info',
+    'license': 'LGPL-3',
+    'depends': ['base', 'web', 'website'],
+    'data': [
+
+    ],
+    'installation': True,
+    'application': True,
+    'auto_install': False,
+    'maintainers': ['Tanvir'],
+}
